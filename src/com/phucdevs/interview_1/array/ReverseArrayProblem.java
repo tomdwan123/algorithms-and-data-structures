a19f6dac377b85c87a4f1313c50ea4266163f8d5
phuc.letan <tomdwanno1@gmail.com>
2023-05-03 13:04:48 +0700

    Reverse array problem exercise

--- src/com/phucdevs/interview_1/array/ReverseArrayProblem.java
+++ src/com/phucdevs/interview_1/array/ReverseArrayProblem.java
@@ -2,9 +2,19 @@ package com.phucdevs.interview_1.array;
 
 public class ReverseArrayProblem {
 
-    public int[] solve(int[] nums) {
+    public static void main(String[] args) {
 
-        // hint: define 2 pointers (pointing to the last and first item of the array) and make a single iteration ...
+        /**
+         * Input: [2, 10, 5, 3, 4, 9, 6, 8]
+         * Output: [8, 6, 9, 4, 3, 5, 10, 2]
+         */
+        int[] nums = {2, 10, 5, 3, 4, 9, 6, 8};
+        solve1(nums);
+        solve2(nums);
+    }
+
+    static int[] solve1(int[] nums) {
+
         int size = nums.length;
         for (int i = 0; i < size / 2; i++) { // 0 - 2
             swap(nums, i, (size - i - 1));
@@ -13,7 +23,21 @@ public class ReverseArrayProblem {
         return nums;
     }
 
-    private void swap(int[] nums, int index1, int index2) {
+    static int[] solve2(int[] nums) {
+
+        int lowIndex = 0;
+        int highIndex = nums.length - 1;
+
+        while (lowIndex < highIndex) {
+            swap(nums, lowIndex, highIndex);
+            lowIndex++;
+            highIndex--;
+        }
+
+        return nums;
+    }
+
+    static void swap(int[] nums, int index1, int index2) {
 
         int temp = nums[index1];
         nums[index1] = nums[index2];
